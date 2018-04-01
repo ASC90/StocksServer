7f00c073e779adb0835e13f72eba925c55774736
Unknown <arnausantoscabre@gmail.com>
2018-04-01 19:06:07 +0200

    Cambios

--- server/server.js
+++ server/server.js
@@ -44,40 +44,44 @@ app.get("/getTickers", function (req, res) {
 });
 let arrHealthyCompanies = [];
 let arrNullCompanies = [];
-setTimeout(() => {
-    let _tickers = jsonTickers;
-    let i = 0
-    let interval = setInterval(() => {
-        console.log('tickers', _tickers[i].symbol);
-        //
-        let companyTicker = _tickers[i].symbol;
-        let obj = functions.functions.reqest("https://www.quandl.com/api/v3/datasets/WIKI/" + _tickers[i].symbol + "/data.json?api_key=" + claves._keys.QUANDL_KEY).then(function (pres) {
-            let company = JSON.parse(pres);
-            if (company.dataset_data) {
-                arrHealthyCompanies.push(companyTicker);
-                fs.writeFile('./JSON_files/' + companyTicker + '.json', pres, function (err) {
+let reqOK = false;
+if (reqOK) {
+    setTimeout(() => {
+        let _tickers = jsonTickers;
+        let i = 0
+        let interval = setInterval(() => {
+            console.log('tickers', _tickers[i].symbol);
+            //
+            let companyTicker = _tickers[i].symbol;
+            let obj = functions.functions.reqest("https://www.quandl.com/api/v3/datasets/WIKI/" + _tickers[i].symbol + "/data.json?api_key=" + claves._keys.QUANDL_KEY).then(function (pres) {
+                let company = JSON.parse(pres);
+                if (company.dataset_data) {
+                    arrHealthyCompanies.push(companyTicker);
+                    fs.writeFile('./JSON_files/' + companyTicker + '.json', pres, function (err) {
+                        if (err) throw err;
+                        //console.log('Saved!');
+                    });
+                } else {
+                    arrNullCompanies.push(companyTicker);
+                }
+            });
+            //
+            i++;
+            if (i > _tickers.length - 1) {
+                console.log('end');
+                fs.writeFile('./JSON_files/notFound.json', arrNullCompanies, function (err) {
                     if (err) throw err;
                     //console.log('Saved!');
                 });
-            } else {
-                arrNullCompanies.push(companyTicker);
+                clearInterval(interval);
             }
-        });
-        //
-        i++;
-        if (i > _tickers.length) {
-            console.log('end');
-            fs.writeFile('./JSON_files/notFound.json', arrNullCompanies, function (err) {
-                if (err) throw err;
-                //console.log('Saved!');
-            });
-            clearInterval(interval);
-        }
-    }, 1500);
-}, 1000);
+        }, 1500);
+    }, 1000);
+}
 
 app.get("/test", function (req, res) {
-    res.send({a: arrHealthyCompanies, b: arrNullCompanies});
+    reqOK = true;
+    res.send({ a: arrHealthyCompanies, b: arrNullCompanies });
 });
 
 
