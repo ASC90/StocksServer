dee920d664cd2df8d555286097a09e947a214e4d
Unknown <arnausantoscabre@gmail.com>
2018-04-01 17:00:19 +0200

    Intentando guardar archivo

---
+++ server/functions-for-server.js
@@ -0,0 +1,24 @@
+var https = require("https");
+var functions = {
+    reqest: asyncReq
+}
+
+// Request
+function req(options) {
+    let body = "";
+    return new Promise(resolve => {
+        https.get(options, function (res) {
+            res.on("data", data => {
+                body += data;
+                resolve(body);
+            });
+        }) 
+    });
+}
+
+async function asyncReq(options) {
+    let resp = await req(options);
+    return resp;
+}
+
+exports.functions = functions;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var fs = require('fs');
 var bodyParser = require('body-parser');
 const cors = require("cors");
-const keys = require('./keys');
+const claves = require('./keys');
+const functions = require('./functions-for-server');
 var app = express();
 
 app.use(cors({
@@ -15,3 +16,18 @@ app.listen(8080, () => {
 });
 
 // Get
+app.get("/getTickers", function (req, res) {
+    var options = 'https://api.iextrading.com/1.0/ref-data/symbols';
+    // let jsonObj = JSON.stringify(functions.functions.reqest(options));
+    // res.send(jsonObj);
+    /*let obj = functions.functions.reqest(options);
+    setTimeout(() => {
+        console.log('obj', obj);
+    }, 10000);*/
+    res.send(fs.writeFile('./tickers.json', functions.functions.reqest(options), function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    }));
+});
+
+
