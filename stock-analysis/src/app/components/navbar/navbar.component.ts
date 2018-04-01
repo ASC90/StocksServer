c3f20efe58ef1b7f19c6e9a79435a29134016946
Unknown <arnausantoscabre@gmail.com>
2018-04-01 23:10:36 +0200

    Materialize

---
+++ stock-analysis/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {MaterializeDirective} from "angular2-materialize";
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
