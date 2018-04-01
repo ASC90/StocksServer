82ae96c57d40899c9cd38cf05408482c366bd8b8
Unknown <arnausantoscabre@gmail.com>
2018-04-01 19:01:16 +0200

    Servidor que captura json de las empresas

--- server/functions-for-server.js
+++ server/functions-for-server.js
@@ -1,7 +1,6 @@
 var https = require("https");
 var functions = {
-    reqest: asyncReq,
-    hola: saluda
+    reqest: asyncReq
 }
 
 // Request
@@ -24,11 +23,6 @@ async function asyncReq(options) {
     let resp = await req(options);
     return resp;
 }
-// Test
-function saluda() {
-    return 'hola';
-}
-// Get all companies
 
 
 exports.functions = functions;
--- server/server.js
+++ server/server.js
@@ -4,9 +4,16 @@ var bodyParser = require('body-parser');
 const cors = require("cors");
 const claves = require('./keys');
 const functions = require('./functions-for-server');
+var jsonTickers;
 var app = express();
+// const path = require('path');
 
-let arrayHolas = [];
+fs.stat('./JSON_files/tickers.json', function (err, stat) {
+    if (err == null) {
+        console.log('File exists');
+        jsonTickers = require('./JSON_files/tickers.json');
+    }
+});
 
 app.use(cors({
     methods: ["POST"]
@@ -17,39 +24,60 @@ app.listen(8080, () => {
     console.log("listening at http://localhost:8080");
 });
 
-// Get
+// Get tickers
 app.get("/getTickers", function (req, res) {
-    var options = 'https://api.iextrading.com/1.0/ref-data/symbols';
+    let options = 'https://api.iextrading.com/1.0/ref-data/symbols';
     // let jsonObj = JSON.stringify(functions.functions.reqest(options));
     // res.send(jsonObj);
     /*let obj = functions.functions.reqest(options);
     setTimeout(() => {
         console.log('obj', obj);
     }, 10000);*/
-    let obj = functions.functions.reqest(options).then(function(pres){
+    let obj = functions.functions.reqest(options).then(function (pres) {
         let tickers = JSON.parse(pres);
-        console.log(arrayHolas);
-        let i = 0
-        let arr = [];
-        let interval = setInterval(() => {
-            console.log('tickers', tickers[i].symbol);
-            arr.push(tickers[i].symbol);
-            i ++;
-            if (i > 10) {
-                console.log('end');
-                res.send(arr);
-                clearInterval(interval);
-            }
-        }, 10);
-        fs.writeFile('./tickers.json', pres, function (err) {
+        res.send(tickers);
+        fs.writeFile('./JSON_files/tickers.json', pres, function (err) {
             if (err) throw err;
             //console.log('Saved!');
         });
     });
 });
+let arrHealthyCompanies = [];
+let arrNullCompanies = [];
+setTimeout(() => {
+    let _tickers = jsonTickers;
+    let i = 0
+    let interval = setInterval(() => {
+        console.log('tickers', _tickers[i].symbol);
+        //
+        let companyTicker = _tickers[i].symbol;
+        let obj = functions.functions.reqest("https://www.quandl.com/api/v3/datasets/WIKI/" + _tickers[i].symbol + "/data.json?api_key=" + claves._keys.QUANDL_KEY).then(function (pres) {
+            let company = JSON.parse(pres);
+            if (company.dataset_data) {
+                arrHealthyCompanies.push(companyTicker);
+                fs.writeFile('./JSON_files/' + companyTicker + '.json', pres, function (err) {
+                    if (err) throw err;
+                    //console.log('Saved!');
+                });
+            } else {
+                arrNullCompanies.push(companyTicker);
+            }
+        });
+        //
+        i++;
+        if (i > _tickers.length) {
+            console.log('end');
+            fs.writeFile('./JSON_files/notFound.json', arrNullCompanies, function (err) {
+                if (err) throw err;
+                //console.log('Saved!');
+            });
+            clearInterval(interval);
+        }
+    }, 1500);
+}, 1000);
 
-for (let i = 0; i < 50; i++) {
-    arrayHolas.push(functions.functions.hola());
-}
+app.get("/test", function (req, res) {
+    res.send({a: arrHealthyCompanies, b: arrNullCompanies});
+});
 
 
