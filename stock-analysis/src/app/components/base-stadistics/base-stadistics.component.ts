4f3c1ce1b03a49252df5cca776924abc7433fe8d
Unknown <arnausantoscabre@gmail.com>
2018-04-08 15:55:14 +0200

    sort table

--- stock-analysis/src/app/components/base-stadistics/base-stadistics.component.ts
+++ stock-analysis/src/app/components/base-stadistics/base-stadistics.component.ts
@@ -9,10 +9,15 @@ import { StatisticsService } from '../../services/statistics.service';
 export class BaseStadisticsComponent implements OnInit {
   list = [];
   tickerList = [];
-  // datatable
+  // table
   rows = [];
-  columns = [{ticker: 'ticker', data: '% from max'}];
+  columns = { ticker: 'ticker', data: '% from max' };
+  // charge
   isDataLoaded = false;
+  percentDataLoaded = 0;
+  // sort table
+  sortMax = 0;
+  sortTicker = 0;
   constructor(
     private service: StatisticsService
   ) { }
@@ -23,14 +28,15 @@ export class BaseStadisticsComponent implements OnInit {
       for (let index = 0; index < res.length; index++) {
         let ticker = res[index];
         this.service.getDataByTicker(ticker).subscribe(data => {
-          this.rows.push({ ticker: ticker, data: this.historicMax(data)});
+          this.rows.push({ ticker: ticker, data: this.historicMax(data) });
           this.list.push({ ticker: ticker, data: this.historicMax(data) });
           this.list.sort(function (a, b) { return a.data - b.data });
+          this.percentDataLoaded = Math.round((index / (res.length - 1)) * 100);
           if (index == res.length - 1) {
             this.isDataLoaded = true;
           }
         });
-      } 
+      }
     });
   }
   historicMax(data) {
@@ -45,5 +51,45 @@ export class BaseStadisticsComponent implements OnInit {
     }
     return Math.abs(((actualPrice / maximusPrice) * 100) - 100);
   }
+  sortByMax() {
+    this.sortMax++;
+    if (this.sortMax > 2) {
+      this.sortMax = 0;
+    }
+    switch (this.sortMax) {
+      case 0:
 
+        break;
+      case 1:
+        this.rows.sort(function (a, b) { return a.data - b.data });
+        break;
+      case 2:
+      this.rows.sort(function (a, b) { return b.data - a.data });
+        break;
+      default:
+
+        break;
+    }
+    
+  }
+  sortByTicker() {
+    this.sortTicker++;
+    if (this.sortTicker > 2) {
+      this.sortTicker = 0;
+    }
+    switch (this.sortTicker) {
+      case 0:
+
+        break;
+      case 1:
+        this.rows.sort(function (a, b) { return a.ticker - b.ticker });
+        break;
+      case 2:
+      this.rows.sort(function (a, b) { return b.ticker - a.ticker });
+        break;
+      default:
+
+        break;
+    }
+  }
 }
